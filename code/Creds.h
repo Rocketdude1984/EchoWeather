ed0f35af92b5fd2cc67e6a239d8892acfe3ed60e
Jake Cornelius <jakecornds@gmail.com>
2025-11-03 14:23:01 -0600

    Uploading libraries and Cred file

---
+++ code/Creds.h
@@ -0,0 +1,18 @@
+// credentials.h
+#ifndef CREDS_H
+#define CREDS_H
+
+const char* WIFI_SSID = "yourSSID";
+const char* WIFI_PASSWORD = "yourPassword";
+// Google Project ID
+const char* PROJECT_ID = "yourProjectID";
+// Service Account's client email
+const char* CLIENT_EMAIL = "yourClientEmail";
+// Service Account's private key
+const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----\yourPrivateKey\n-----END PRIVATE KEY-----\n";
+// The ID of the spreadsheet where you'll publish the data
+const char spreadsheetId[] = "yourSpreadSheetID";
+// Location of Station
+const char location[] = "LocationOfYourStation";
+
+#endif
